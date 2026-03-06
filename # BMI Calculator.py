4a77c4ab4496425b7a7c1600b655053c9b8fbc70
Vamshi <vamshimudugula8@gmail.com>
2026-03-06 15:36:34 +0530

    Add files via upload

---
+++ # BMI Calculator.py
@@ -0,0 +1,17 @@
+# BMI Calculator
+
+weight = float(input("Enter your weight (kg): "))
+height = float(input("Enter your height (meters): "))
+
+bmi = weight / (height * height)
+
+print("Your BMI is:", round(bmi,2))
+
+if bmi < 18.5:
+    print("Underweight")
+elif bmi < 25:
+    print("Normal weight")
+elif bmi < 30:
+    print("Overweight")
+else:
+    print("Obese")
